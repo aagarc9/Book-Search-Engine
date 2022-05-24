05da4e09527164fa3e6889bc1e372d038479b869
Andrew Garcia <andrewagarcia8@gmail.com>
2022-05-23 17:16:41 -0700

    deployment

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ async function runApolloServer() {
 
   db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`Now listening on localhost:${PORT}`);
+      console.log(`🌍 Now listening on localhost:${PORT}`);
       console.log(
         `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
       );
